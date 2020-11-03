73fce3880b698ff3465b3a11ba5545157cd652ec
Manisha-RamakrishnaG-au8 <manishagramakrishna@gmail.com>
2020-11-03 09:45:41 -0800

    day 01 cc with driver codev

--- coding-challenges/week19/day 02/q1.py
+++ coding-challenges/week19/day 02/q1.py
@@ -1,4 +1,13 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        x = "".join(i for i in x if i.isalnum())
-        return x.lower() == x[::-1].lower()
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        mystring = s.lower()
+        mystring2 = ""
+        for i in mystring:
+            if i.isalnum():
+                mystring2 += i
+        return mystring2
+    
+        for i in range(0, int(len(mystring2)/2)):  
+            if mystring2[i] != mystring2[len(mystring2)-i-1]: 
+                return False
+        return True
